fac02c73555bea9fde8a4b582a770de882f0b107
Garrett Biernat <garrett@gdbmedia.net>
2016-05-05 19:23:23 -0700

    working heroku

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,5 @@
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-235-207-226.compute-1.amazonaws.com:5432/postgresql-rigid-80428?sslmode=require&user=dgbqlsgcrhgvxg&password=B4qckTv_SB182u41uFQfDrbQEB", null, null);
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-235-207-226.compute-1.amazonaws.com:5432/d9kvmr2rao0eq0?sslmode=require&user=dgbqlsgcrhgvxg&password=B4qckTv_SB182u41uFQfDrbQEB", null, null);
 }
