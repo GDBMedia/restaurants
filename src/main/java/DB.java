d3976db9e77535f3c69241488dc5faae873262ed
Garrett Biernat <garrett@gdbmedia.net>
2016-05-05 19:03:28 -0700

    trying heroku sql

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,5 @@
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/rest", null, null);
+  public static Sql2o sql2o = new Sql2o("postgres://dgbqlsgcrhgvxg:B4qckTv_SB182u41uFQfDrbQEB@ec2-54-235-207-226.compute-1.amazonaws.com:5432/d9kvmr2rao0eq0", null, null);
 }
